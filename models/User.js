3079b3064d4bb91f5287492a5e29b10304d40944
Aandal Priya <aandalpriya94@gmail.com>
2025-07-12 15:15:16 +0530

    Fix signup server error

--- models/User.js
+++ models/User.js
@@ -2,13 +2,19 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true,
-    unique: true // ✅ Only email should be unique
+    unique: true
   },
-  password: { type: String, required: true }
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
